902dc9482c65f8eaba60ac3ed74dac2e2973fe93
AidasLit <aidaslit@gmail.com>
2025-05-06 08:15:59 +0300

    added a button to add a Notif to the database

--- app/src/main/java/com/example/labworks/AlarmFragment.kt
+++ app/src/main/java/com/example/labworks/AlarmFragment.kt
@@ -1,11 +1,10 @@
 package com.example.labworks
 
-import android.app.Dialog
-import android.content.Intent
-import android.media.RingtoneManager
-import android.net.Uri
+import android.icu.text.CaseMap.Title
 import android.os.Bundle
-import androidx.activity.viewModels
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -13,33 +12,31 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.labworks.database.NotifViewModel
 import com.example.labworks.database.data.Notif
-import androidx.compose.ui.window.Dialog
 
 class AlarmFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: android.view.LayoutInflater, container: android.view.ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): android.view.View {
+    ): View {
         return ComposeView(requireContext()).apply {
             setContent {
                 AlarmScreen()
@@ -68,85 +65,95 @@ fun AlarmScreen(
                 .padding(top = 32.dp)
         )
 
-        // Apvalus + mygtukas apačioje
-        Box(
-            contentAlignment = Alignment.Center,
+        NotifButton(
+            viewModel,
             modifier = Modifier
-                .size(64.dp)
-                .background(Color(0xFF333333), shape = CircleShape)
-                .align(Alignment.BottomCenter)
-                .padding(8.dp)
-                .clickable {
-                    // TODO: Čia įdėsi, ką veiks mygtukas
+                .align(Alignment.BottomEnd)
+                .padding(32.dp)
+        )
+    }
+}
 
-//                    NotifCreateDialog(
-//                        {},
-//                        {}
-//                    )
+@Composable
+fun NotifButton(viewModel: NotifViewModel, modifier: Modifier){
+    val showDialog = remember { mutableStateOf(false) }
 
-                    val newNotif = Notif("temp")
+    if (showDialog.value) {
+        NotifCreateDialog(
+            onDismissRequest = { showDialog.value = false },
+            onConfirmation = { title ->
+                val newNotif = Notif(title)
+                viewModel.addNotif(newNotif)
 
-                    viewModel.addNotif(newNotif)
+                showDialog.value = false
+            }
+        )
+    }
 
-                    println(newNotif.id)
-
-                    /*viewModel.addDescriptionComponent(
-                        newNotif,
-                        title = "test component title",
-                        description = "test description"
-                    )*/
-                }
-        ) {
-            Text(
-                text = "+",
-                color = Color.White,
-                fontSize = 32.sp,
-                textAlign = TextAlign.Center
-            )
-        }
+    FloatingActionButton(
+        onClick = { showDialog.value = true },
+        containerColor = Color(0xFF333333),
+        contentColor = Color.White,
+        modifier = modifier
+    ) {
+        Icon(
+            Icons.Filled.Add,
+            contentDescription = "Add a Ping",
+            tint = Color.White
+        )
     }
 }
 
 @Composable
 fun NotifCreateDialog(
-    onDismissRequest: () -> Unit,
-    onConfirmation: () -> Unit
+    onDismissRequest: () -> Unit = {},
+    onConfirmation: (title : String) -> Unit = {}
 ) {
+    var newTitle by remember { mutableStateOf("") }
+
     Dialog(onDismissRequest = { onDismissRequest() }) {
         // Draw a rectangle shape with rounded corners inside the dialog
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(375.dp)
+                .wrapContentHeight()
                 .padding(16.dp),
             shape = RoundedCornerShape(16.dp),
         ) {
             Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
+                verticalArrangement = Arrangement.spacedBy(8.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
+
+                modifier = Modifier.padding(16.dp)
             ) {
                 Text(
-                    text = "This is a dialog with buttons and an image.",
-                    modifier = Modifier.padding(16.dp),
+                    text = "Create a new Ping",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
                 )
+
+                OutlinedTextField(
+                    value = newTitle,
+                    onValueChange = { newTitle = it },
+                    singleLine = true,
+                    label = { Text("Enter Ping name") }
+                )
+
                 Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
                 ) {
                     TextButton(
                         onClick = { onDismissRequest() },
-                        modifier = Modifier.padding(8.dp),
+                        modifier = Modifier.padding(horizontal = 8.dp),
                     ) {
-                        Text("Dismiss")
+                        Text("Cancel")
                     }
                     TextButton(
-                        onClick = { onConfirmation() },
-                        modifier = Modifier.padding(8.dp),
+                        onClick = { onConfirmation(newTitle) },
+                        modifier = Modifier.padding(horizontal = 8.dp),
                     ) {
-                        Text("Confirm")
+                        Text("Add")
                     }
                 }
             }
