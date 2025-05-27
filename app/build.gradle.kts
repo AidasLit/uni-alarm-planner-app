8968926cab54018354c284a1a9ef1fea7ad8ba9b
Sh1mmer1 <juozukaskav1@gmail.com>
2025-05-28 01:10:23 +0300

    pridetas map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.guava:guava:31.1-android")
+    implementation("com.google.android.gms:play-services-maps:19.2.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.datastore:datastore-core-android:1.1.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
